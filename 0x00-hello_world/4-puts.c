360df71a038515996746bcc36d8ac4bd1ff27405
bruce <brucenelm@gmail.com>
2021-08-26 12:03:21 -0700

    Added Comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * The is the beginnining of the program execution
+ * main - Entry point
  *
- * The Function always returns 0
+ * Return: Always 0 (Success)
  */
-
-int main()
-{	
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
