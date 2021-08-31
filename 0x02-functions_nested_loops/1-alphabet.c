45402c9da108c3ad62ddcad8f358123b97f19b14
bruce <brucenelm@gmail.com>
2021-08-31 10:43:46 -0700

    created 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always void (Success)
+ */
+void print_alphabet(void)
+{
+	for (int i = 'a'; i<='z'; i++)
+	{
+		_putchar(n);
+	
+	}     	
+	_putchar("\n");
+}
