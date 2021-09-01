5e2f66cbfde50ecea4e4e82de52ef6fdb0dfb346
bruce <brucenelm@gmail.com>
2021-08-31 17:46:18 -0700

    fixed declaration statements

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,12 +2,13 @@
 
 void print_times_table(int n)
 {
+	int i, j;
 	if (n >= 0 && n <= 15)
 	{
-	    for(int j = 0; j <= n; j++)
+	    for(j = 0; j <= n; j++)
 	    {
 	    
-	    for(int i = 0; i <= n;i++)
+	    for(i = 0; i <= n;i++)
 	    {
 	        int x = j * i;
 	        
