36e5ecbbc6605bd95c85ef700f6eb845e615d813
bruce <brucenelm@gmail.com>
2021-09-02 08:15:46 -0700

    created main.h,_putchar and 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - check if character is uppercase
+ * @a: character to check
+ *
+ * Return: 1 if c is uppercase,
+ * 0 otherwise.
+ */
+int _isupper(int a)
+{
+	return (a >= 'A' && a <= 'Z');
+}
