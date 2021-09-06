d9e55c6ce646b3be5d438ef512303b3415cf3aaa
bruce <brucenelm@gmail.com>
2021-09-06 11:38:11 -0700

    betty

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_array - print array
+ * _strcpy - copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest.
  * @dest: character pointer
  * @src: character source
  *
